6b626f21f55a7cb07a91f301c444e55c7acb4bd8
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-11-28 23:55:57 +0530

    ex-03 wiring button click

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 //alert("Hello ji!")
 //const a = prompt("ENter a value of z");
 //alert(`a value is ${a}`);
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+    console.log("button clicked ");
+})
